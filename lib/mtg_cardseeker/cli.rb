fe319c1c00d2245b918bc784a068b1f5acd7b8fe
bebofofum <bri.boehm@gmail.com>
2020-07-17 21:23:48 -0400

    Added some calls to colorize method for strings. Adjusted the range validity check for prompt_by type. Added a range local variable.

--- lib/mtg_cardseeker/cli.rb
+++ lib/mtg_cardseeker/cli.rb
@@ -4,7 +4,8 @@ module MtgCardseeker
   class CLI
 
     def start
-      puts "Welcome to the Card Seeker. Choose a type of card:"
+      puts
+      puts "Welcome to the Card Seeker. Let's look at some cards!".colorize(:green)
       @input = nil
       menu
       get_input
@@ -12,14 +13,17 @@ module MtgCardseeker
     end
 
     def menu
-      # This uses a HEREDOC for multiline listing 
+      # This uses a HEREDOC for multiline listing
+      puts "-------------------------".colorize(:green) 
       puts <<-MENU
-      1. See cards by Name 
-      2. See cards by Creature 
-      3. See cards by Enchantment 
-      4. See cards by Sorcery
 
-      Or type 'exit' at any time to leave the program. Type 'menu' to return to the main menu.
+Choose a how you would like to see a card. You can view by name, creature, enchantment or sorcery:
+1. See cards by Name 
+2. See cards by Creature 
+3. See cards by Enchantment 
+4. See cards by Sorcery
+
+Or type 'exit' at any time to leave the program. Type 'menu' to return to the main menu.
       MENU
     end
 
@@ -34,28 +38,28 @@ module MtgCardseeker
           list_sorted_by_name
           prompt_and_display_card_choice
           puts
-          puts "Would you like to see another card? Type 'menu' to see options or 'exit' to end."
-        elsif @input ==  "2"
+          puts "Type" + " 'menu'".colorize(:green) + " to see options or " + "'exit'".colorize(:green) + " to end."
+        elsif @input == "2"
           list_sorted_by("Creature")
           prompt_and_display_type("Creature")
           puts
-          puts "Would you like to see another card? Type 'menu' to see options or 'exit' to end."
-        elsif @input ==  "3"
+          puts "Type" + " 'menu'".colorize(:green) + " to see options or " + "'exit'".colorize(:green) + " to end."
+        elsif @input == "3"
           list_sorted_by("Enchantment")
           prompt_and_display_type("Enchantment")
           puts
-          puts "Would you like to see another card? Type 'menu' to see options or 'exit' to end."
-        elsif @input ==  "4"
+          puts "Type" + " 'menu'".colorize(:green) + " to see options or " + "'exit'".colorize(:green) + " to end."
+        elsif @input == "4"
           list_sorted_by("Sorcery")
           prompt_and_display_type("Sorcery")
           puts
-          puts "Would you like to see another card? Type 'menu' to see options or 'exit' to end."
+          puts "Type" + " 'menu'".colorize(:green) + " to see options or " + "'exit'".colorize(:green) + " to end."
         elsif @input ==  "menu"
           menu
         elsif @input == "exit" || @input == "quit"
           
         else
-          puts "Sorry, I didn't get that. Here are the options again."
+          puts "Sorry, I didn't get that. Here are the options again.".colorize(:red)
           menu  
         end
       end
@@ -75,11 +79,13 @@ module MtgCardseeker
     # ---List section---
 
     def list_sorted_by_name
+      puts "-------------------------".colorize(:green)
       card_sorted = MtgCards.all_uniq.take(20).sort_by {|card_array| card_array.name}
       card_sorted.each.with_index(1) {|card, index| puts "#{index}. #{card.name}"}
     end
 
     def list_sorted_by(type)
+      puts "-------------------------".colorize(:green)
       cards_sorted = MtgCards.find_card_by_type(type).sort_by {|card_array| card_array.name}
       cards_sorted.each.with_index(1) {|card, index| puts "#{index}. #{card.name}"}
     end
@@ -87,37 +93,45 @@ module MtgCardseeker
     # ---Prompt section---
 
     def prompt_and_display_card_choice
+      puts "-------------------------".colorize(:green)
       puts "Enter the card number would you like to see more details about."
       @input = gets.strip.downcase
       index = @input.to_i - 1
       if (1..20).include?(index) 
         if index >= 0
-          puts "Name: #{MtgCards.all_uniq[index].name}"
-          puts "Color: #{MtgCards.all_uniq[index].colors.join}"
-          puts "ManaCost: #{MtgCards.all_uniq[index].manacost}"
-          puts "Type: #{MtgCards.all_uniq[index].types.join}"
-          puts "Description: #{MtgCards.all_uniq[index].text}"
+          puts "Name: ".colorize(:green) + "#{MtgCards.all_uniq[index].name}"
+          puts "Color: ".colorize(:green) + "#{MtgCards.all_uniq[index].colors.join}"
+          puts "ManaCost: ".colorize(:green) + "#{MtgCards.all_uniq[index].manacost}"
+          puts "Type: ".colorize(:green) + "#{MtgCards.all_uniq[index].types.join}"
+          puts "Description: ".colorize(:green) + "#{MtgCards.all_uniq[index].text}"
         end
       else
-        puts "Sorry, that's not a valid entry. Please pick a number 1-20."
+        puts "Sorry, that's not a valid entry.".colorize(:red)
+        puts
+        # menu 
       end
     end
 
     def prompt_and_display_type(type)
+      puts "-------------------------".colorize(:green)
       puts "Enter the card number would you like to see more details about."
       @input = gets.strip.downcase
       index = @input.to_i - 1
-      if (1..20).include?(index) 
+      range = MtgCards.find_card_by_type(type).length - 1
+      # binding.pry
+      if (1..range).include?(index) 
         if index >= 0
-          card = MtgCards.find_card_by_type(type)[index]
-          puts "Name: #{card.name}"
-          puts "Color: #{card.colors.join}"
-          puts "ManaCost: #{card.manacost}"
-          puts "Type: #{card.types.join}"
-          puts "Description: #{card.text}"
+        card = MtgCards.find_card_by_type(type)[index]
+        puts "Name: ".colorize(:green) + "#{card.name}"
+        puts "Color: ".colorize(:green) + "#{card.colors.join}"
+        puts "ManaCost: ".colorize(:green) + "#{card.manacost}"
+        puts "Type: ".colorize(:green) + "#{card.types.join}"
+        puts "Description: ".colorize(:green) + "#{card.text}"
         end
       else
-        puts "Sorry, that's not a valid entry. Please pick a number 1-20."
+        puts "Sorry, that's not a valid entry".colorize(:red)
+        puts
+        # menu 
       end
     end
 
